44d7fe331c50fd40d6d2471b3ae762cad3cec98c
tintinve <pedr0261@stud.kea.dk>
2018-08-30 11:39:20 +0200

    w1d4 - solution V1.0

---
+++ w1d4 - modal/mysolution/script.js
@@ -0,0 +1,13 @@
+"use strict";
+let modal_container = document.querySelector("#modal_container");
+let closeButton = document.querySelector("#closeButton");
+document.querySelectorAll("article a").forEach(link => {
+  link.addEventListener("click", showModal);
+});
+function showModal() {
+  modal_container.classList.remove("hidden");
+}
+closeButton.addEventListener("click", closeModal);
+function closeModal() {
+  modal_container.classList.add("hidden");
+}
