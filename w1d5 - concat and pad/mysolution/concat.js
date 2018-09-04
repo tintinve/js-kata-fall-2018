b2aca7c1653fdc5b02e2e4f8468fec5e476649d8
tintinve <pedroluismejia@gmail.com>
2018-09-04 15:17:56 +0200

    w1d5 - solution

--- w1d5 - concat and pad/mysolution/concat.js
+++ w1d5 - concat and pad/mysolution/concat.js
@@ -1,22 +1,4 @@
-let name = "Pedro";
-const len = name.length;
-let str = "           Pedro";
-let parag = "The quick brown fox jumped over the lazy dog. If the dog barked";
-let searchTerm = "quick brown";
-let indexOfFirst = parag.indexOf(searchTerm);
-let indexSubstring = parag.substr(indexOfFirst, parag.length);
-let change = "dog";
 let filename = "image#.jpg";
 let changeChar = "#";
-
-console.log(`${name}`);
-console.log(len);
-console.log(name[0]);
-console.log(str.trim());
-console.log(indexOfFirst);
-console.log(indexSubstring);
-console.log(parag.replace(change, "broder"));
-
 // Solution 1git
 console.log(filename.replace(changeChar, "3"));
-console.log(name[0]);
